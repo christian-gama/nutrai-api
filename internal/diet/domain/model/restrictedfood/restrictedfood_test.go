b5c516004a3406e0a673e087b6193c671c727d61
André Oliveira <andreos@fieb.org.br>
2023-05-15 16:56:14 -0300

    fix(restrictedfood): rename test function

--- internal/diet/domain/model/restrictedfood/restrictedfood_test.go
+++ internal/diet/domain/model/restrictedfood/restrictedfood_test.go
@@ -16,7 +16,7 @@ func TestRestrictedFoodSuite(t *testing.T) {
 	suite.RunUnitTest(t, new(RestrictedFoodTestSuite))
 }
 
-func (s *RestrictedFoodTestSuite) TestErrorNewRestrictedFood() {
+func (s *RestrictedFoodTestSuite) TestNewRestrictedFood() {
 	type Sut struct {
 		Sut  func() (*restrictedfood.RestrictedFood, error)
 		Data *restrictedfood.RestrictedFood
@@ -35,37 +35,39 @@ func (s *RestrictedFoodTestSuite) TestErrorNewRestrictedFood() {
 		return &Sut{Sut: sut, Data: data}
 	}
 
-	s.Run("ID", func() {
-		s.Run("Should return an error when empty", func() {
-			sut := makeSut()
-			sut.Data.ID = 0
+	s.Run("TestNewRestrictedFood (Error)", func() {
+		s.Run("ID", func() {
+			s.Run("Should return an error when empty", func() {
+				sut := makeSut()
+				sut.Data.ID = 0
 
-			restrictedfood, err := sut.Sut()
+				restrictedfood, err := sut.Sut()
 
-			s.ErrorAsRequired(err)
-			s.Nil(restrictedfood)
-		})
-	})
-
-	s.Run("Name", func() {
-		s.Run("Should return an error when empty", func() {
-			sut := makeSut()
-			sut.Data.Name = ""
-
-			restrictedfood, err := sut.Sut()
-
-			s.ErrorAsRequired(err)
-			s.Nil(restrictedfood)
+				s.ErrorAsRequired(err)
+				s.Nil(restrictedfood)
+			})
 		})
 
-		s.Run("Should return an error when invalid", func() {
-			sut := makeSut()
-			sut.Data.Name = "invalid"
+		s.Run("Name", func() {
+			s.Run("Should return an error when empty", func() {
+				sut := makeSut()
+				sut.Data.Name = ""
 
-			restrictedfood, err := sut.Sut()
+				restrictedfood, err := sut.Sut()
 
-			s.ErrorAsRequired(err)
-			s.Nil(restrictedfood)
+				s.ErrorAsRequired(err)
+				s.Nil(restrictedfood)
+			})
+
+			s.Run("Should return an error when invalid", func() {
+				sut := makeSut()
+				sut.Data.Name = "invalid"
+
+				restrictedfood, err := sut.Sut()
+
+				s.ErrorAsRequired(err)
+				s.Nil(restrictedfood)
+			})
 		})
 	})
 }
