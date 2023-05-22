43acdd178adb807b825702b78e7b915b6bd21f60
Christian Gama <christiangama.dev@gmail.com>
2023-05-22 20:48:35 -0300

    test: create tests with todo msg

---
+++ internal/auth/api/http/middleware/auth_handler_test.go
@@ -0,0 +1,16 @@
+package middleware_test
+
+import (
+	"testing"
+
+	"github.com/christian-gama/nutrai-api/testutils/suite"
+)
+
+type AuthHandlerSuite struct {
+	suite.Suite
+}
+
+func TestAuthHandlerSuite(t *testing.T) {
+	t.Skipf("TODO: implement me")
+	suite.RunUnitTest(t, new(AuthHandlerSuite))
+}
